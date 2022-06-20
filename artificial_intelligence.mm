13c448958fd3e6226e3bbd0bfab64948d43b3186
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-06-20 15:04:58 -0500

    Create artificial_intelligence.mm

---
+++ artificial_intelligence.mm
@@ -0,0 +1,120 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Artificial Intelligence" FOLDED="false" ID="ID_2545853" CREATED="1655501666483" MODIFIED="1655504890407" STYLE="oval">
+<font SIZE="12"/>
+<hook NAME="MapStyle" layout="OUTLINE">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="Data engineer" POSITION="right" ID="ID_895403077" CREATED="1655501827258" MODIFIED="1655504617219">
+<edge COLOR="#ff0000"/>
+<font SIZE="12"/>
+<node TEXT="Extract" ID="ID_803005817" CREATED="1655504919743" MODIFIED="1655504928899">
+<font SIZE="12"/>
+</node>
+<node TEXT="Transform" ID="ID_1472136686" CREATED="1655504933807" MODIFIED="1655504941142">
+<font SIZE="12"/>
+</node>
+<node TEXT="Load" ID="ID_174701183" CREATED="1655504944826" MODIFIED="1655504949843">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Analytics expert" POSITION="left" ID="ID_1328671951" CREATED="1655501843573" MODIFIED="1655504623022">
+<edge COLOR="#0000ff"/>
+<font SIZE="12"/>
+</node>
+<node TEXT="Statistician" POSITION="right" ID="ID_44683282" CREATED="1655501852170" MODIFIED="1655504627498">
+<edge COLOR="#00ff00"/>
+<font SIZE="12"/>
+</node>
+<node TEXT="Machine learning engineer" POSITION="left" ID="ID_1691581709" CREATED="1655501862794" MODIFIED="1655504631317">
+<edge COLOR="#ff00ff"/>
+<font SIZE="12"/>
+</node>
+<node TEXT="Data scientist" POSITION="right" ID="ID_37647330" CREATED="1655501876026" MODIFIED="1655504636804">
+<edge COLOR="#00ffff"/>
+<font SIZE="12"/>
+</node>
+<node TEXT="Data science architect" POSITION="left" ID="ID_854841172" CREATED="1655501891570" MODIFIED="1655504640536">
+<edge COLOR="#7c0000"/>
+<font SIZE="12"/>
+</node>
+</node>
+</map>
